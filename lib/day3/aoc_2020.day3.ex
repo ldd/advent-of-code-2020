9d28bd7f7956af8f8a3cd50e5f4c53e80b05d206
ldd <leonardoldd@gmail.com>
2020-12-03 01:14:21 -0500

    feat: solve day 3

---
+++ lib/day3/aoc_2020.day3.ex
@@ -0,0 +1,24 @@
+defmodule AOC_2020.Day3 do
+  import AOC_2020.Utils
+
+  def count_trees(input, slope, rows) do
+    input
+    |> Enum.with_index()
+    |> Enum.filter(fn {_r, i} -> rem(i, rows) == 0 end)
+    |> Enum.map(fn {r, i} ->
+      location = rem(floor(i * slope), String.length(r))
+      String.graphemes(r) |> Enum.at(location)
+    end)
+    |> valid_count(&(&1 == "#"))
+  end
+
+  def part1(input) do
+    count_trees(input, 3, 1)
+  end
+
+  def part2(input) do
+    [[1, 1], [3, 1], [5, 1], [7, 1], [0.5, 2]]
+    |> Enum.map(fn [slope, r] -> count_trees(input, slope, r) end)
+    |> multiply()
+  end
+end
